1521165fee5895a86a29537d196d1f613f273d85
Marfi098776 <mabrurgotech@gmail.com>
2026-01-04 15:13:29 +0600

    added CRUD

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
-import express, { Request, Response } from "express"
+import express, { NextFunction, Request, Response } from "express"
 import {Pool} from "pg";
 import dotenv from "dotenv";
 import path from "path";
+import { setDefaultCACertificates } from "tls";
+import { title } from "process";
 
 dotenv.config({ path: path.join(process.cwd(), ".env") });
 
@@ -46,9 +48,13 @@ const initDB = async() => {
 }
 initDB()
 
+//logger middleware
+const logger = (req: Request, res: Response, next: NextFunction) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
+    next();
+}
 
-
-app.get('/', (req: Request, res: Response) => {
+app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello Next LEVEL developers!')
 })
 
@@ -71,7 +77,7 @@ app.post("/users", async(req: Request, res: Response) => {
     }
 })
 
-app.get("/users", async(req: Request, res: Response) => {
+app.get("/users", logger, async(req: Request, res: Response) => {
     try{
         const result = await pool.query(`SELECT * FROM users`);
 
@@ -163,7 +169,124 @@ app.delete("/users/:id", async(req: Request, res: Response) => {
 })
 
 // todos CRUD
+app.post("/todos", async(req: Request, res: Response) => {
+    const { user_id, title } = req.body;
 
+    try{
+        const result = await pool.query(`INSERT INTO todos(user_id, title) VALUES($1, $2) RETURNING *`, [user_id, title]);
+        res.status(201).json({
+            success: true,
+            message: "todo created",
+            data: result.rows[0],
+        })
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+
+    }
+})
+
+app.get("/todos", async(req: Request, res: Response) => {
+    try{
+        const result = await pool.query(`SELECT * FROM todos`);
+
+        res.status(200).json({
+            success: true,
+            message: "todos retrieved successfully",
+            data: result.rows,
+        })
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+            details: err
+        })
+    }
+})
+
+app.get("/todos/:id", async(req: Request, res: Response) => {
+    try{
+        const result = await pool.query(`SELECT * FROM todos WHERE id = $1`, [req.params.id]);
+
+        if(result.rows.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "todos not found",
+            });
+        }else{
+            res.status(200).json({
+                success: true,
+                message: "todos fetched successfully",
+                data: result.rows[0],
+            })
+        }
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+})
+
+app.put("/todos/:id", async(req: Request, res: Response) => {
+    const { user_id, title } = req.body;
+    console.log("user_id: ", user_id, "title: ", title);
+    try{
+        const result = await pool.query(`UPDATE todos SET user_id=$1, title=$2 WHERE id=$3 RETURNING *`, [user_id, title, req.params.id]);
+
+        if(result.rows.length === 0){
+            res.status(404).json({
+                success: false,
+                message: "todo not found",
+            })
+        } else{
+            res.status(200).json({
+                success: true,
+                message: "todo updated successfully",
+                data: result.rows[0],
+            });
+        }
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+})
+
+app.delete("/todos/:id", async(req: Request, res: Response) => {
+    try{
+        const result = await pool.query(`DELETE FROM todos WHERE id = $1`, [req.params.id]);
+
+        if(result.rowCount === 0) {
+            res.status(404).json({
+                success: false,
+                message: "todo not found",
+            });
+        }else{
+            res.status(200).json({
+                success: true,
+                message: "todo deleted successfully",
+                data: result.rows,
+            })
+        }
+    }catch(err: any){
+        res.status(500).json({
+            success: false,
+            message: err.message,
+        })
+    }
+})
+
+app.use((req, res) => {
+    res.status(404).json({
+        success: false,
+        message: "Route not found",
+        path: req.path,
+    })
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
