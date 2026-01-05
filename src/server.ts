60a484a7c46f28a7a79dd77c1f60947fc2ee11b1
Marfi098776 <mabrurgotech@gmail.com>
2026-01-05 11:59:20 +0600

    added moduler pattern

--- src/server.ts
+++ src/server.ts
@@ -1,81 +1,42 @@
 import express, { NextFunction, Request, Response } from "express"
-import {Pool} from "pg";
-import dotenv from "dotenv";
-import path from "path";
-import { setDefaultCACertificates } from "tls";
-import { title } from "process";
-
-dotenv.config({ path: path.join(process.cwd(), ".env") });
+import config from "./config";
+import initDB, { pool } from "./config/db";
+import logger from "./middleware/logger";
+import { userRoutes } from "./modules/user/user.routes";
 
 const app = express();
-const port = 5000;
+const port = config.port;
 
 // parser
 app.use(express.json())
 
-// DB
-const pool = new Pool({
-    connectionString: `${process.env.CONNECTION_STR}`
-})
-
-const initDB = async() => {
-    await pool.query(`
-        CREATE TABLE IF NOT EXISTS users(
-        id SERIAL PRIMARY KEY,
-        name VARCHAR(100) NOT NULL,
-        email VARCHAR(150) UNIQUE NOT NULL,
-        age INT,
-        phone VARCHAR(15),
-        address TEXT,
-        created_at TIMESTAMP DEFAULT NOW(),
-        updated_at TIMESTAMP DEFAULT NOW()
-        )
-        `);
-        
-        
-        await pool.query(`
-            CREATE TABLE IF NOT EXISTS todos(
-            id SERIAL PRIMARY KEY,
-            user_id INT REFERENCES users(id) ON DELETE CASCADE,
-            title VARCHAR(200) NOT NULL,
-            description TEXT,
-            completed BOOLEAN DEFAULT false,
-            due_date DATE,
-            created_at TIMESTAMP DEFAULT NOW(),
-            updated_at TIMESTAMP DEFAULT NOW()
-            )
-            `);
-}
+// initializing DB
 initDB()
 
-//logger middleware
-const logger = (req: Request, res: Response, next: NextFunction) => {
-    console.log(`[${new Date().toISOString()}] ${req.method} ${req.path}\n`);
-    next();
-}
-
+// logger middleware
 app.get('/', logger, (req: Request, res: Response) => {
   res.send('Hello Next LEVEL developers!')
 })
 
 // users CRUD
-app.post("/users", async(req: Request, res: Response) => {
-    const {name, email} = req.body;
+app.use("/users", userRoutes);
+// app.post("/users", async(req: Request, res: Response) => {
+//     const {name, email} = req.body;
 
-    try{
-        const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
-        res.status(201).json({
-            success: false,
-            message: "Data Inserted successfully",
-            data: result.rows[0],
-        });
-    }catch(err: any){
-        res.status(500).json({
-            success: false,
-            message: err.message,
-        });
-    }
-})
+//     try{
+//         const result = await pool.query(`INSERT INTO users(name, email) VALUES($1, $2) RETURNING *`, [name, email]);
+//         res.status(201).json({
+//             success: false,
+//             message: "Data Inserted successfully",
+//             data: result.rows[0],
+//         });
+//     }catch(err: any){
+//         res.status(500).json({
+//             success: false,
+//             message: err.message,
+//         });
+//     }
+// })
 
 app.get("/users", logger, async(req: Request, res: Response) => {
     try{
